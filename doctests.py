ff0b6b67c0979f5a0d0e4b16b69f46482a987b26
pgregory <paddygregory62@gmail.com>
2025-07-11 16:00:03 +1000

    added unittest examples

--- doctests.py
+++ doctests.py
@@ -5,3 +5,22 @@ from doctest import run_docstring_examples
 def run_doctests(func):
     run_docstring_examples(func, globals(), name=func.__name__)
 
+# unittest
+
+def add(a,b):
+    return a+b
+from unittest import TestCase
+
+
+class TestExample(TestCase):
+
+    def test_add(self):
+        result = add(2,2)
+        self.assertEqual(result, 100)
+
+import unittest
+
+def run_unittest():
+    unittest.main(argv=[''], verbosity=0, exit=False)
+
+run_unittest()
